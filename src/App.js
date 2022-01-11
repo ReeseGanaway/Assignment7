3a2e9b681334a0986becaa00d23ab9e0eba5e488
Reese Ganaway <reeseganaway@gmail.com>
2022-01-11 16:42:39 -0500

    Fetched the home page but its getting too many gifs, not sure how to limit it.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react'
+import React, {useEffect, useState } from 'react'
+import GifCard from "./components/GifCard"
+import SearchField from "./components/SearchField"
 
 
 
-console.log(process.env.REACT_APP_API_KEY)
+
 export default function App() {
+
+  const trendingUrl=`http://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_API_KEY}`
+
+  const [loading, setLoading]= useState(true)
+  const [gifs,setGifs]=useState([])
+  const [searchText,setSearchText] = useState("")
+  
+
+  const fetchGif= async () =>{
+    try{
+      const response= await fetch(trendingUrl);
+      const data= await response.json()
+      setGifs(data)
+      setLoading(false)
+      console.log(gifs.data[0].url)
+    } catch(error){
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchGif();
+  }, [gifs])
+
+  const onChange = (e) =>{
+    setSearchText(e.target.value)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='homeGIFs'>
+        {loading===false ? gifs.img:<p>Loading</p>}
       </header>
+      <SearchField onChange={onChange} />
     </div>
   );
 }
