a97af936357599ea4c37777976eb6c8718bdeb80
Reese Ganaway <reeseganaway@gmail.com>
2022-01-13 00:32:53 -0500

    Search function is now working completely, and when search bar is empty, the page should be displaying trending gifs, unless the user randomized the page (randomize feature in next commit)

--- src/App.js
+++ src/App.js
@@ -1,88 +1,66 @@
-import './App.css';
-import React, {useEffect, useState } from 'react'
-import GifCard from "./components/GifCard"
-import SearchField from "./components/SearchField"
-
-
-
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import GifCard from "./components/GifCard";
+import SearchField from "./components/SearchField";
 
 export default function App() {
+  const trendingUrl = `http://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_API_KEY}`;
+  const [loading, setLoading] = useState(true);
+  const [gifs, setGifs] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-  const trendingUrl=`http://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_API_KEY}`
-  const [loading, setLoading]= useState(true)
-  const [gifs,setGifs]=useState([])
-  const [searchText,setSearchText] = useState("")
-  
-
-  const fetchTrendingGif= async()=> {
-    try{
-      const response= await fetch(trendingUrl);
-      const trendingData= await response.json()
-      setGifs(trendingData.data)
+  const fetchTrendingGif = async () => {
+    try {
+      const response = await fetch(trendingUrl);
+      const trendingData = await response.json();
+      setGifs(trendingData.data);
       //console.log(gifs)
-      setLoading(false)
-    } catch(error){
-      console.log(error)
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
     }
-  }
-  
-  /*const fetchSearchedGif= async()=> {
-      try{
-        const response= await fetch(searchUrl);
-        const trendingData= await response.json()
-        setGifs(trendingData.data)
-        console.log(gifs)
-        setLoading(false)
-      } catch(error){
-        console.log(error)
-      }
-    }*/
+  };
 
-  
-
-  useEffect( () => {
+  useEffect(() => {
     fetchTrendingGif();
     return () => {
       setGifs([]);
     };
-  }, [])
+  }, []);
 
-  const onChange = (e) =>{
-    console.log(e.target.value)
-    if(!e.target.value){
-      
-      document.querySelector('.TrendingHeader').innerHTML="Trending!";
+  const onChange = (e) => {
+    console.log(e.target.value);
+    if (!e.target.value) {
+      document.querySelector(".TrendingHeader").innerHTML = "Trending!";
       fetchTrendingGif();
+    } else {
+      const fetchSearchedGif = async () => {
+        try {
+          const response = await fetch(
+            `http://api.giphy.com/v1/gifs/search?q=${e.target.value}&api_key=${process.env.REACT_APP_API_KEY}`
+          );
+          const searchData = await response.json();
+          setGifs(searchData.data);
+          //console.log('Hi',gifs)
+          setLoading(false);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      fetchSearchedGif();
+      document.querySelector(".TrendingHeader").innerHTML = "Results";
     }
-    const fetchSearchedGif= async()=>{
-    try{
-      const response= await fetch(`http://api.giphy.com/v1/gifs/search?q=${e.target.value}&api_key=${process.env.REACT_APP_API_KEY}`);
-      const searchData= await response.json()
-      setGifs(searchData.data)
-      //console.log('Hi',gifs)
-      setLoading(false)
-    } catch(error){
-      console.log(error)
-    }}
-    fetchSearchedGif();
-    document.querySelector('.TrendingHeader').innerHTML="Results";
+  };
 
-
-  }
-  
   return (
     <div className="App">
-      <h1 className='TrendingHeader'>Trending!
-      </h1>
-      <SearchField className='SearchField' onChange={onChange}/>
+      <h1 className="TrendingHeader">Trending!</h1>
+      <SearchField className="SearchField" onChange={onChange} />
       <div className="Gif-Box">
-      {gifs.map((x)=>(
-         <GifCard className="Gifs" url={x.images.original.url}/>))}
+        {gifs.map((x) => (
+          <GifCard className="Gifs" url={x.images.original.url} />
+        ))}
       </div>
-      
     </div>
   );
 }
-
-
-
